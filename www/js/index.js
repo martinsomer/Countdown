3ab384004e0dd7e2b6d2b82ac8a032a41c1cda9b
martinsomer <martinsom35@gmail.com>
2019-04-05 05:03:14 +0300

    Basic local storage functionality

--- www/js/index.js
+++ www/js/index.js
@@ -4,42 +4,71 @@
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
+ * 'License'); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 var app = {
-    // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+//        this.saveData('test', '0')
+        this.refreshList()
     },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    
+    retrieveData: function() {
+        if (localStorage.getItem('items') !== null) {
+            
+            for (let i=0; i<JSON.parse(localStorage.getItem('items')).length; i++) {
+                
+                let title = document.createElement('span')
+                title.innerHTML = JSON.parse(localStorage.getItem('items'))[i][0]
+                title.setAttribute('class', 'title')
+                
+                let date = document.createElement('span')
+                date.innerHTML = JSON.parse(localStorage.getItem('items'))[i][1]
+                date.setAttribute('class', 'date')
+                
+                let item = document.createElement('div')
+                item.setAttribute('class', 'item')
+                item.appendChild(title)
+                item.appendChild(date)
+                document.getElementById('items').appendChild(item)
+            }
+        }
+    },
+    
+    saveData: function(title, date) {
+        let items = []
+        const item = [title, date]
+        
+        if (localStorage.getItem('items') !== null) {
+            items = JSON.parse(localStorage.getItem('items'))
+        }
+        items.push(item)
+        localStorage.setItem('items', JSON.stringify(items))
+    },
+    
+    removeData: function(item) {
+        localStorage.removeItem(item)
+    },
+    
+    refreshList: function() {
+        const items = document.getElementById('items')
+        while (items.firstChild) {
+            items.removeChild(items.firstChild)
+        }
+        this.retrieveData()
     }
 };
 
